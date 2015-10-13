796e875ebba109870227f91a6ea5873fe3384afc
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-10-13 11:58:49 -0400

    OpenLevelManager added
    Just type the name of your scene into the open level manager in the
    LoaddLevel component. Add it to your scene and make sure to add your
    scene to the build settings. <---This is the most important

---
+++ ProjectOfficeBreakout/Assets/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public string[] sceneNames;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int index = -1;
+	    if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            print("I Was Here");
+            index = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            index = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            index = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            index = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            index = 4;
+        }
+
+        if (index >= 0 && index < sceneNames.Length)
+        {
+            Application.LoadLevel(sceneNames[index]);
+        }
+	}
+}
