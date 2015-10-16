8d1af465e53a33f19d5987eaecbf648e9b507cbf
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-10-16 00:15:42 -0400

    AI and sound behavior scripts added to Ryan Demo

---
+++ ProjectOfficeBreakout/Assets/RyanFolder/Scripts/ArtificialIntelligence/StateMachine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour {
+    public State[] validStates;
+    State currentState;
+
+    protected virtual void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        currentState.updateState(Time.deltaTime);
+    }
+
+    public void changeState(State newState)
+    {
+        currentState.exitState();
+        currentState = newState;
+        currentState.enterState();
+    }
+}
